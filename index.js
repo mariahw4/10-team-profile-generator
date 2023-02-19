235e09e80926bb8339302226631fe7c21980df06
Mariah Wear <wearmariahk@gmail.com>
2023-02-18 21:01:07 -0700

    fixing inquirer questions so prompts occur and html is generated

--- index.js
+++ index.js
@@ -1,49 +1,92 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
+const fs = require("fs");
 
 const generateHTML = require("./src/generateHTML");
 const generateCSS = require("./src/generateCSS");
 
+// const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-inquirer
-  .prompt([
+const employeeQuestions = [
     {
       type: 'input',
       message: 'What is your name?',
-      name: 'name',
-    },
-    {
-        type: "list",
-        message: "What is your role?",
-        name: "role",
-        choices: ["Employee", "Engineer", "Manager", "Intern"],
+      name: 'employeeName',
     },
     {
       type: 'input',
       message: 'What is your id?',
-      name: 'id',
+      name: 'employeeId',
     },
     {
       type: 'input',
       message: 'What is your email?',
-      name: 'email',
+      name: 'employeeEmail',
     },
     {
-        type: "input",
-        message: "What is your office number?",
-        name: "officeNumber",
-    },
-    {
-        type: "input",
-        message: "What is your github username?",
-        name: "github",
-    },
-    {
-        type: "input",
-        message: "What school do you attend?",
-        name: "school",
-    },
-  ])
+    type: "list",
+    message: "What is your role?",
+    name: "employeeRole",
+    choices: ["Engineer", "Manager", "Intern"], 
+    },   
+// ]
+// if (data.employeeRole === "Manager") {
+//     const managerQuestions = [
+        {
+            type: "input",
+            message: "What is your office number?",
+            name: "officeNumber",
+        },
+//     ]
+// }
+// if (data.employeeRole === "Engineer") {
+//     const engineerQuestions = [
+        {
+            type: "input",
+            message: "What is your github username?",
+            name: "employeeGithub",
+        },
+//     ]
+// }
+// if (data.employeeRole === "Intern") {
+//     const internQuestions = [
+        {
+            type: "input",
+            message: "What school do you attend?",
+            name: "employeeSchool",
+        },
+    ]
+// }  
+function getName(value) {
+    return `${value}`
+}
+
+function getId(value) {
+    return `${value}`
+}
+
+function getEmail(value) {
+    return `${value}`
+}
+
+function getRole(value) {
+    return `${value}`
+};
+
+function getGithub(value) {
+    return `${value}`
+}
+
+function getOfficeNumber(value) {
+    return `${value}`
+}
+
+function getSchool(value) {
+    return `${value}`
+}
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateHTML(data), function (err) {
@@ -61,16 +104,24 @@ function writeToFile(fileName, data) {
   }
 
   function init() {
-    inquirer.prompt(questions).then((data) => {
+    // Employee();
+    // Manager();
+    // Engineer();
+    // Intern();
+    inquirer.prompt(employeeQuestions).then((data) => {
         console.log(JSON.stringify(data, null, " "));
-        // data.getLicense = getLicense(data.license);
-        // data.licenseSelection = licenseSelection(data.license);
-        // data.githubLink = githubLink(data.userName);
-        // data.emailTo = emailTo(data.userEmail);
+        data.getName = getName(data.employeeName);
+        data.getId = getId(data.employeeId);
+        data.getEmail = getEmail(data.employeeEmail);
+        data.getRole = getRole(data.employeeRole);
+        data.getGithub = getGithub(data.employeeGithub);
+        data.getOfficeNumber = getOfficeNumber(data.officeNumber);
+        data.getSchool = getSchool(data.employeeSchool);
         writeToFile("./dist/index.html", data);
         writeToStyle("./dist/style.css", data);
-    });
   }
+    )};
+  
   
   // // Function call to initialize app
   init();
