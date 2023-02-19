c4ab1e448b3cf87041c8c7cd548a470a833cc14c
Mariah Wear <wearmariahk@gmail.com>
2023-02-18 19:37:14 -0700

    updating template to be generated

--- src/generateCSS.js
+++ src/generateCSS.js
@@ -1,5 +1,16 @@
 
 function generateCSS(data) {
+    return `
+header {
+    background-color: red;
+    color: white;
+    text-align: center;
+    height: 100px;
+}
+
+
+
+`
 
 
 }
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,6 +1,49 @@
 
 function generateHTML(data) {
+    return `
+<!DOCTYPE html>
+<html lang="en-US">
 
+  <head>
+    <meta charset="UTF-8">
+    <title>Team Info</title>
+    <link rel="stylesheet" href="./style.css">
+  </head>
+
+  <body>
+    <header>
+      <!-- Headers -->
+      <h1>My Team</h1>
+    </header>
+
+    <section>
+        <div>
+            <h2>Employee Name ${data.employeeName}</h2>
+            <h3>Employee Role ${data.employeeRole} </h3>
+        </div>
+
+        <div>
+            <ul>
+                <li>ID</li>
+                <li>Email</li>
+                <li> github or office number </li>
+            </ul>
+        </div>
+
+    </section>
+
+    
+  </body>
+
+</html>
+
+
+
+
+
+
+
+`
 
 }
 
