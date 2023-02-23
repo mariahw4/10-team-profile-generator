6faaa09650b265f1390e7b2a1227729bb6d3940d
Mariah Wear <wearmariahk@gmail.com>
2023-02-23 13:51:24 -0700

    added validation to the inquirer prompts

--- index.js
+++ index.js
@@ -17,21 +17,40 @@ const managerProfile = function() {
       type: 'input',
       message: 'What is the name of the Team Manager?',
       name: 'name',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("Please enter Team Manager Name");
+        }
+        return true;
+      }
     },
     {
       type: 'input',
       message: 'What is the Team Manager ID?',
       name: 'id',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid ID is required.");
+        }
+        return true;
+      }
     },
     {
       type: 'input',
       message: 'What is the email of the Team Manager?',
       name: 'email',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid email is required.");
+        }
+        return true;
+      }
     },
     {
       type: "input",
       message: "What is the Team Manager office number?",
       name: "officeNumber",
+      default: "TBD",
     },
 ])
 .then(managerData => {
@@ -53,28 +72,58 @@ const addTeamMembers = function() {
       type: 'input',
       message: 'What is the name of the employee?',
       name: 'name',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("Please enter Engineer Name");
+        }
+        return true;
+      }
     },
     {
       type: 'input',
      message: 'What is the ID of the employee?',
       name: 'id',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid ID is required.");
+        }
+        return true;
+      }
     },
     {
       type: 'input',
       message: 'What is the email of the employee?',
       name: 'email',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid email is required.");
+        }
+        return true;
+      }
     },
     {
       type: "input",
       message: "What is the github username of the employee?",
       name: "github",
       when: (input) => input.role === 'Engineer',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid github is required.");
+        }
+        return true;
+      }
     },
     {
       type: "input",
       message: "What school does the Intern attend?",
       name: "school",
       when: (input) => input.role === 'Intern',
+      validate: function (answer) {
+        if (answer.length < 1) {
+            return console.log("A valid school is required.");
+        }
+        return true;
+      }
     },
     {
       type: 'confirm',
